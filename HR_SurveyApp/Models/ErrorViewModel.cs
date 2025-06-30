d0462fe610733b22a183a405b4f55d87184ed2a3
kriska-sosiska <keetaezart@gmail.com>
2025-06-30 22:19:22 +0400

    лишнее

--- HR_SurveyApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HR_SurveyApp.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
